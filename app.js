bae277b7eaf3eaf59051427398386cb5702d62d2
davidcassalstudio <davidcassal@gmail.com>
2023-03-13 21:51:48 -0300

    Inserção de DB user e DB pass em hard code

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const path          = require('path');
 const mongoose      = require('mongoose');
 require('dotenv').config()
 
-console.log(process.env.USER)
-console.log(process.env.PASS)
+// console.log(process.env.USER)
+// console.log(process.env.PASS)
 
 // MongoDB Database Connection
 //mongoose.connect('mongodb://127.0.0.1:27017/aguiasmartech')
-mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.akxf9rd.mongodb.net/?retryWrites=true&w=majority`)
+// mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.akxf9rd.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://davidcassal:2wwSRyed2CvH3UpL@cluster0.akxf9rd.mongodb.net/?retryWrites=true&w=majority`)
     .then(response => {
         console.log('Conectado com sucesso!');
     })
