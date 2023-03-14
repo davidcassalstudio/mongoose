1472e793ab493498d610983581599658747cb029
davidcassalstudio <davidcassal@gmail.com>
2023-03-13 22:14:47 -0300

    Alteração na rota da home

--- routes/home/home.js
+++ routes/home/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const homeController = require('../../controller/home.controller');
 
-router.get('/home', homeController.getHome)
+router.get('/', homeController.getHome)
 router.get('/delete-item/:id', homeController.deleteItem)
 
 module.exports = router;
